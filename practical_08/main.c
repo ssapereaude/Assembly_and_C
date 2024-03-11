5c06d705f1050ace48dd79bf1db35cb0426c821e
U-SETU\C00280204 <C00280204@CD-A313-34.setu.ie>
2024-03-11 17:21:03 +0000

    practical_08

---
+++ practical_08/main.c
@@ -0,0 +1,8 @@
+#include "stdio.h" // standard IO header file
+
+// Mainline
+int main()
+{
+    printf("Hello Assembly and C thats practical 01\n"); // Call to printf function
+    return 0;
+}
---
+++ practical_08/practical_08_part1/main.c
@@ -0,0 +1,7 @@
+#include "stdio.h" //standar IO header file
+
+//Mainline
+int main() { 
+printf("Hello Assembly and C\n");// call to printf fucntion
+return 0;
+}
